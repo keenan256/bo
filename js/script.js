dcce6e047b660bc23a3ecaaad2551bb7d70bb935
keenan256 <kakiiki256@gmail.com>
2024-06-27 16:39:54 +0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// scripts.js
+
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Document fully loaded and parsed.');
+});
+
